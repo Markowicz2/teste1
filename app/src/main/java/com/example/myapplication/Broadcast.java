9af65c46fdae721cae8ef99fac4b04d16f38e25c
Rodrigo Duarte Markowicz <rodrigo.markowicz@hexagon.com>
2019-07-04 11:53:23 -0300

    Funcionando Botao

--- app/src/main/java/com/example/myapplication/Broadcast.java
+++ app/src/main/java/com/example/myapplication/Broadcast.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -13,44 +14,58 @@ import android.widget.Toast;
 public class Broadcast extends BroadcastReceiver {
     private String TAG = "HeadSet";
 
-    public Broadcast() {
+    public Broadcast()
+    {super();
         Log.d(TAG, "Created");
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        KeyEvent event = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
-        if (event == null) {
-            return;
-        }
 
-        int action = event.getAction();
-        Toast.makeText(context, "Action lauchched", Toast.LENGTH_SHORT).show();
-        if (action == KeyEvent.ACTION_DOWN) {
+        if(intent.getAction().equals(Intent.ACTION_MEDIA_BUTTON)) {
+
+            KeyEvent event = (KeyEvent) intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+            if (event == null) {
+                return;
+
+            }
+
+            Log.i("TESTE", "Entrou aki");
+            Toast.makeText(context, "NUMERO: " + event.getKeyCode(), Toast.LENGTH_LONG).show();
+            int action = event.getAction();
+
+            Toast.makeText(context, "Action lauchched", Toast.LENGTH_SHORT).show();
+            if (action == KeyEvent.ACTION_DOWN) {
+                // do something
+                Toast.makeText(context, "BUTTON PRESSED!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "teste", Toast.LENGTH_LONG).show();
+                Log.i("TESTE ACTION", intent.getAction());
+            }
             // do something
-            Toast.makeText(context, "BUTTON PRESSED!", Toast.LENGTH_SHORT).show();
-        }
-        // do something
-        if (action == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE)
-            Toast.makeText(context, "Play Pause pressed!", Toast.LENGTH_SHORT).show();
-        Log.i("TESTE", intent.getAction());
+            if (action == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE) {
+                Toast.makeText(context, "Play Pause pressed!", Toast.LENGTH_SHORT).show();
+                Log.i("TESTE ACTION2", intent.getAction());
+            }
 
+        }else if(intent.getAction().equals(Intent.ACTION_HEADSET_PLUG)) {
 
-      /*  if(intent.getAction().equals(AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
-
-            Log.i("TESTE","etste");
             int state = intent.getIntExtra("state", -1);
             switch(state) {
                 case(0):
-                    Log.d(TAG, "Headset unplugged");
+                    Log.d("TESTE", "Headset unplugged");
                     break;
                 case(1):
-                    Log.d(TAG, "Headset plugged");
+                    Log.i("TESTE", "Headset plugged");
                     break;
                 default:
-                    Log.d(TAG, "Error");
+                    Log.i("TESTE", "Error");
             }
+
+        }else{
+
+            Log.i("TESTE","OUTROBROADCAST");
         }
-    */
+        abortBroadcast();
+
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,20 +2,46 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.media.session.MediaSession;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         IntentFilter intent = new IntentFilter(Intent.ACTION_MEDIA_BUTTON);
+        IntentFilter intent2 = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+
+
+
          Broadcast myReceiver = new Broadcast();
 
+
+
+
          registerReceiver(myReceiver,intent);
+         registerReceiver(myReceiver,intent2);
+
+
+
+
+
+
+
+
+
     }
 
 }
